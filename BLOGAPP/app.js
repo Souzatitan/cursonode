b9aced2ae53d5d4c717043ce8a3a645b0a74a57d
Anderson de Souza <asouzanderson@gmail.com>
2024-09-26 11:47:30 -0300

    atualizada

--- BLOGAPP/app.js
+++ BLOGAPP/app.js
@@ -7,6 +7,11 @@ const admin = require('./routes/admin');
 const mongoose = require("mongoose")
 const session = require("express-session")
 const flash = require("connect-flash")
+require("./models/Postagem")
+const Postagem = mongoose.model("postagens")
+require("./models/Categoria")
+const Categoria = mongoose.model("categorias")
+const usuario = require("./routes/usuario")
 
 // Configurações
 //sessão
@@ -46,8 +51,64 @@ const flash = require("connect-flash")
 
 // Rotas
     app.get('/', (req, res) => {
-        res.send('Rota principal');
-});
+        Postagem.find().lean().populate("categoria").sort({data: 'desc'}).then((postagens) => {
+            res.render("index", {postagens: postagens})
+        }).catch((erro)=> {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/404")
+        })
+    })    
+
+    app.get("/postagem/:slug", (req, res)=>{
+        Postagem.findOne({slug: req.params.slug}).lean().then((postagem)=> {
+            if(postagem) {
+                res.render("postagem/index", {postagem: postagem})
+            }else{
+                req.flash("error_msg", "Esta postagem não existe")
+                res.redirect("/")
+            }
+        }).catch((error)=> {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
+    })
+
+    app.get("/categorias", (req, res)=>{
+        Categoria.find().lean().then((categorias)=>{
+            res.render("categorias/index", {categorias: categorias})
+        }).catch((error)=> {
+            req.flash("error_msg", "Houve um erro interno ao listar")
+            res.redirect("/")
+        })    
+    })
+
+    app.get("/categorias/:slug", (req, res)=>{
+        Categoria.findOne({slug: req.params.slug}).then((categoria)=>{
+            if(categoria){
+                 
+                Postagem.find({categoria: categoria._id}).lean().then((postagens)=>{
+
+                    res.render("categorias/postagens", {postagens: postagens, categoria: categoria})
+
+
+                }).catch((error)=>{
+                    req.flash("erro_msg", "Houve um erro interno categorias")
+                    res.redirect("/")
+                })
+
+            }else{
+                req.flash("error_msg", "Essa categoria não exite ")
+                res.redirect("/")
+            }
+        }).catch((error)=> {
+            req.flash("error_msg", "Houve um erro interno categorias")
+            res.redirect("/")
+        })    
+    })
+
+    app.get("/404", (req, res)=> {
+        res.send('Erro 404!')
+    })
 
     app.get('/posts', (req, res) => {
         res.send('Lista de Posts');
@@ -58,8 +119,11 @@ const flash = require("connect-flash")
     app.get('/test-bootstrap', (req, res) => {
         res.render('test-bootstrap');
 });
+    app.use('/admin', admin)
+    app.use("/usuarios", usuarios)
 
-    const PORT = 8081;
-    app.listen(PORT, () => {
-        console.log(`Servidor Rodando na porta ${PORT}`);
+//Outros
+const PORT = 8081;
+app.listen(PORT, () => {
+    console.log(`Servidor Rodando na porta ${PORT}`);
 });
--- BLOGAPP/routes/admin.js
+++ BLOGAPP/routes/admin.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require("mongoose")
 require("../models/Categoria")
 const Categoria = mongoose.model("categorias")
+require('../models/Postagem')
+const Postagem = mongoose.model("postagens")
 
 router.get('/', (req, res) => {
     res.render("admin/index");
@@ -24,7 +26,7 @@ router.get('/categorias', (req, res) => {
 
 router.get('/categorias/add', (req, res) => {
     res.render("admin/addcategorias");
-});
+})
 router.post("/categorias/nova", (req, res) => {
 
     var erros = []
@@ -104,5 +106,112 @@ router.post("/categorias/deletar", (req, res) => {
     })    
 
 })    
+
+router.get("/postagens", (req, res) => {
+
+    Postagem.find().lean().populate("categoria").sort({data:"desc"}).then((postagens)=> {
+        res.render("admin/postagens", {postagens: postagens})
+    }).catch((erro) => {
+        console.log(erro)
+        req.flash("error_msg", "Houve um erro ao listar postagens ")
+        res.redirect("/admin")    
+    })  
+    
+})
+
+router.get("/postagens/add", (req, res) => {
+    Categoria.find().lean().then((categorias)=> {
+        res.render("admin/addpostagem", {categorias: categorias })
+    }).catch((erro) => {
+        req.flash("error_msg", "Houve um erro ao caerregarr")
+        res.redirect("/admin")    
+    })  
+    
+})
+
+router.post("/postagens/nova", (req, res)=> {
+
+    var erros = []
+
+    if(req.body.categoria == "0"){
+        erros.push({texto: "Categoria invalida, registre uma categoria"})
+    }
+
+    if(erros.length > 0) {
+        res.render("admin/addpostagem", {erros: erros})
+    }else{
+        const novaPostagem = {
+            titulo: req.body.titulo,
+            descrição: req.body.descrição,
+            conteudo: req.body.conteudo,
+            categoria: req.body.categoria,
+            slug: req.body.slug
+        }
+
+        new Postagem(novaPostagem).save().then(() => {
+            req.flash("success_msg","Postagem criado com sucesso")
+            res.redirect("/admin/postagens")
+        }).catch((erro) => {
+            req.flash("error_msg", "Houve um erro durante o salvamento ")
+            res.redirect("/admin/postagens")
+        })
+    }
+
+})
+
+router.get("/postagens/edit/:id", (req, res)=> {
+
+    Postagem.findOne({_id:req.params.id}).lean().then((postagem)=>{
+
+        Categoria.find().lean().then((categorias)=>{
+            res.render("admin/editpostagens", {categorias: categorias, postagem: postagem})
+        }).catch((erro) => {
+            req.flash("error_msg", "Erro ao listar")
+            res.redirect("/admin/postagens")
+        
+        })
+    }).catch((erro) => {
+        req.flash("error_msg", "Erro ao carregar")
+        res.redirect("/admin/postagens")
+    })
+
+})
+
+
+router.post("/postagens/edit", (req, res)=> {
+    Postagem.findOne({_id:req.body.id}).then((postagem)=> {
+
+            postagem.titulo = req.body.titulo
+            postagem.descrição = req.body.descrição
+            postagem.conteudo = req.body.conteudo
+            postagem.categoria = req.body.categoria
+            postagem.slug = req.body.slug
+
+            postagem.save().then(()=> {
+                req.flash("success_msg","Postagem editada com sucesso")
+                res.redirect("/admin/postagens")
+            }).catch((erro) => {
+                req.flash("error_msg", "Houve interno ")
+                res.redirect("/admin/postagens")
+
+            })
+        }).catch((erro) => {
+            console.log(erro)
+            req.flash("error_msg", "Erro ao salvar edição")
+            res.redirect("/admin/postagens")
+        })
+})
+
+router.get("/postagens/deletar/:id", (req, res) => {
+    Postagem.deleteOne({_id: req.params.id}).lean().then(() => {
+        req.flash("success_msg", "Postagem deletada")
+        res.redirect("/admin/postagens")
+    }).catch((erro) => {
+        console.log(erro)
+        req.flash("error_msg", "Erro interno")
+        res.redirect("/admin/postagens")
+    })    
+} )
+
 
 module.exports = router
